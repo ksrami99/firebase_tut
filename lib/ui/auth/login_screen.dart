63ed8ac26465493c74ae9cc49518081be300e366
Karan <ksrami99@gmail.com>
2023-08-31 01:54:24 +0530

    Connected Firebase And Crated Login Screen

---
+++ lib/ui/auth/login_screen.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_tut/widgets/round_button.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Login"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Emali',
+                      prefixIcon: Icon(Icons.alternate_email),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter Email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'PassWord',
+                      prefixIcon: Icon(Icons.alternate_email),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter Email';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            RoundButton(
+              title: 'Login',
+              onTap: () {
+                if (_formKey.currentState!.validate()) {}
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
